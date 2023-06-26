147c53268b4376a8f4802ee46bb09a33a9503016
Hritik Anand <realhga@gmail.com:--global user.email>
2023-06-26 15:47:18 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import 'package:service/pages/start.dart';
+
+void main () {
+  runApp(MaterialApp(
+    home: StartPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
